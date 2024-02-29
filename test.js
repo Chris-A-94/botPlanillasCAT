5cb8881a5e17f5aaf4e1a4d3cecef15f3888134e
Chris <almeidachristian94@hotmail.com>
2024-02-29 11:22:20 -0300

    Add object to manipulate Drive folder, and another to manipulate emails
    Objects are incomplete, so far, they can copy an excel file and email it

--- test.js
+++ test.js
@@ -2,4 +2,49 @@ import 'google-apps-script';
 //Google Apps needs the empty function to work
 function myFunction() {}
 
+//this is the mail related object, it will send the completed sheets to final recipient
+const mailFetcher = (function(){
+    let fileToSend;
+    const setAttachment = (attach)=> {
+      fileToSend = DriveApp.getFileById(attach);
+    }
+    const sendMail = (address,subject,body,Name) => {
+      
+      GmailApp.sendEmail(address,subject,body,{
+        attachments: [fileToSend.getAs(MimeType.CSV)],
+        name: Name
+      });
+    };
+    return {sendMail,setAttachment}
+  })();
 
+//drive related object, it will fetch CSV files when they're uploaded
+const driveFolder = (function(){
+    let sheet;
+    const setFile = (ID)=> sheet = DriveApp.getFileById(ID);
+    const getFile = ()=> sheet;
+    // function that creates a dinamic CSV file, copies sheet into it, and returns it
+    const createCopyFile = ()=> {   
+      
+      let parsedData = Utilities.parseCsv(sheet.getBlob().getDataAsString());
+      let changedData = '';
+      for (let i = 0; i < parsedData.length; i++) {
+        changedData += parsedData[i].join(',') + '\n';
+      }
+      let newFile = DriveApp.createFile('Original reading',changedData,MimeType.CSV);   
+     return newFile.getId();
+      //note: once the mail is sent, you should delete the new file
+    }
+  
+    return {getFile,setFile,createCopyFile};
+  })();
+
+  
+
+driveFolder.setFile('1Fzm-pnhaOkcP4n1T3sZMASDuquYHvASr');
+
+
+let attatchment = driveFolder.createCopyFile();
+
+mailFetcher.setAttachment(attatchment);
+mailFetcher.sendMail('oscar6494@gmail.com','Test file', 'Check attactchment');
