dc5c8a946b3b91afba82291d58ab441544b0baaa
Chris <almeidachristian94@hotmail.com>
2024-02-29 15:16:30 -0300

    Add findDataFromApp funtion
    Function goes through the raw CSV and extracts the average values, returning them in an array

--- test.js
+++ test.js
@@ -21,6 +21,7 @@ const mailFetcher = (function(){
 //drive related object, it will fetch CSV files when they're uploaded
 const driveFolder = (function(){
     let sheet;
+    const templateSheet = DriveApp.getFileById('1tAJq_4vK0Vfi_ATQdaPtk1skqc6X0-oheAyrVaux1_w');
     const setFile = (ID)=> sheet = DriveApp.getFileById(ID);
     const getFile = ()=> sheet;
     // function that creates a dinamic CSV file, copies sheet into it, and returns it
@@ -35,16 +36,35 @@ const driveFolder = (function(){
      return newFile.getId();
       //note: once the mail is sent, you should delete the new file
     }
-  
-    return {getFile,setFile,createCopyFile};
+    const loadDataFromApp = ()=> {
+        let originalData = Utilities.parseCsv(sheet.getBlob().getDataAsString());
+        const recordedValues = [];
+        let foundEnd = false;
+        for(let i = 0; i < originalData.length; i++)
+        {
+          for(let j = 0; j < originalData[i].length; j++)
+          {
+            console.log(originalData[i][j]);
+            if(originalData[i][j] === 'Máximo total')
+            {
+              foundEnd = true;
+              break;
+            }
+              
+          }
+          if(foundEnd)
+            break;
+        }
+    }
+    return {getFile,setFile,createCopyFile,loadDataFromApp};
   })();
 
   
 
 driveFolder.setFile('1Fzm-pnhaOkcP4n1T3sZMASDuquYHvASr');
+driveFolder.loadDataFromApp();
 
-
-let attatchment = driveFolder.createCopyFile();
+/*let attatchment = driveFolder.createCopyFile();
 
 mailFetcher.setAttachment(attatchment);
-mailFetcher.sendMail('oscar6494@gmail.com','Test file', 'Check attactchment');
+mailFetcher.sendMail('oscar6494@gmail.com','Test file', 'Check attactchment');*/
