647f6eab618873db91a63d63bbf4c7d8fb281d2a
Chris <almeidachristian94@hotmail.com>
2024-03-11 11:06:30 -0300

    Add insertarFormulas function to calculate data

--- test.js
+++ test.js
@@ -10,122 +10,138 @@ function myFunction() {}
 
 //this is the mail related object, it will send the completed sheets to final recipient
 const mailFetcher = (function(){
-    let fileToSend;
-    const setAttachment = (attach)=> {
-      fileToSend = DriveApp.getFileById(attach);
-    }
-    const sendMail = (address,subject,body,Name,modifiedFile) => {
+  let fileToSend;
+  const setAttachment = (attach)=> {
+    fileToSend = DriveApp.getFileById(attach);
+  }
+  const sendMail = (address,subject,body,Name,modifiedFile) => {
+    
+    GmailApp.sendEmail(address,subject,body,{
+      attachments: [
+        fileToSend.getAs(MimeType.CSV),
+        modifiedFile
+      ],
       
-      GmailApp.sendEmail(address,subject,body,{
-        attachments: [
-          fileToSend.getAs(MimeType.CSV),
-          modifiedFile
-        ],
-        
-        name: Name
-      });
+      name: Name
+    });
 
-     // tempFile.setTrashed(true); this is how you may delete stuff alter
-     driveFolder.deleteTemporaryFiles(fileToSend);
-    };
-    return {sendMail,setAttachment}
-  })();
+   // tempFile.setTrashed(true); this is how you may delete stuff alter
+   driveFolder.deleteTemporaryFiles(fileToSend);
+  };
+  return {sendMail,setAttachment}
+})();
 
 //drive related object, it will fetch CSV files when they're uploaded
 const driveFolder = (function(){
-    let sheet;
-    let secondAttachment;
-    const templateSheet = DriveApp.getFileById('1tAJq_4vK0Vfi_ATQdaPtk1skqc6X0-oheAyrVaux1_w');
-    const setFile = (ID)=> sheet = DriveApp.getFileById(ID);
-    const getFile = ()=> sheet;
-    // function that creates a dinamic CSV file, copies sheet into it, and returns it
-    const createCopyFile = ()=> {   
-      
-      let parsedData = Utilities.parseCsv(sheet.getBlob().getDataAsString());
-      let changedData = '';
-      for (let i = 0; i < parsedData.length; i++) {
-        changedData += parsedData[i].join(',') + '\n';
-      }
-      let newFile = DriveApp.createFile('Original reading',changedData,MimeType.CSV);   
-     return newFile.getId();
-      //note: once the mail is sent, you should delete the new file
+  let sheet;
+  let secondAttachment;
+  const templateSheet = DriveApp.getFileById('1tAJq_4vK0Vfi_ATQdaPtk1skqc6X0-oheAyrVaux1_w');
+  const setFile = (ID)=> sheet = DriveApp.getFileById(ID);
+  const getFile = ()=> sheet;
+  // function that creates a dinamic CSV file, copies sheet into it, and returns it
+  const createCopyFile = ()=> {   
+    
+    let parsedData = Utilities.parseCsv(sheet.getBlob().getDataAsString());
+    let changedData = '';
+    for (let i = 0; i < parsedData.length; i++) {
+      changedData += parsedData[i].join(',') + '\n';
     }
-    //function that finds all the average values from the raw data and returns them in an array
-    const findDataFromApp = ()=> {
-        let originalData = Utilities.parseCsv(sheet.getBlob().getDataAsString());
-        const recordedValues = [];
-        let foundEnd = false;
-        for(let i = 0; i < originalData.length; i++)
+    let newFile = DriveApp.createFile('Original reading',changedData,MimeType.CSV);   
+   return newFile.getId();
+    //note: once the mail is sent, you should delete the new file
+  }
+  //function that finds all the average values from the raw data and returns them in an array
+  const findDataFromApp = ()=> {
+      let originalData = Utilities.parseCsv(sheet.getBlob().getDataAsString());
+      const recordedValues = [];
+      let foundEnd = false;
+      for(let i = 0; i < originalData.length; i++)
+      {
+        for(let j = 0; j < originalData[i].length; j++)
         {
-          for(let j = 0; j < originalData[i].length; j++)
+          if(originalData[i][j] === 'Ø')
+            recordedValues.push(originalData[i][j+2]);            
+          if(originalData[i][j] === 'Máximo total')
           {
-            if(originalData[i][j] === 'Ø')
-              recordedValues.push(originalData[i][j+2]);            
-            if(originalData[i][j] === 'Máximo total')
-            {
-              foundEnd = true;
-              break;
-            }
-              
-          }
-          if(foundEnd)
+            foundEnd = true;
             break;
+          }
+            
         }
-        return recordedValues;
-    }
-    const loadDataFromApp = ()=> {
-      const valuesToUse = findDataFromApp();
-      const newSheet = templateSheet.makeCopy('Medicion Hoy');
-      const editingSheet = SpreadsheetApp.openById(newSheet.getId()).getActiveSheet();
-
-      const valuesArray = [];
-     /* for (let i = 0; i < valuesToUse.length; i += 2) {
-          valuesArray.push([valuesToUse[i], valuesToUse[i + 1]]);
-      }*/
-      
-      
-      if(valuesToUse.length % 2 === 0)
-      {
-        for (let i = 0; i < valuesToUse.length/2; i ++) 
-          valuesArray.push([valuesToUse[i], valuesToUse[i + valuesToUse.length/2]]);
+        if(foundEnd)
+          break;
       }
-      else
-      {
-        for (let i = 0; i < valuesToUse.length/2; i ++) 
-          valuesArray.push([valuesToUse[i], valuesToUse[i + 1 + valuesToUse.length/2]]);
-        valuesArray.push([valuesToUse[valuesToUse.length], 0]);
-      }        
+      return recordedValues;
+  }
 
-      let range = editingSheet.getRange('D9:E14');
-      range.setValues(valuesArray);
-      SpreadsheetApp.flush();
-      const returnFile = DriveApp.getFileById(newSheet.getId());
-      return returnFile;
+  const insertarFormulas = (arrayOfValues) => {
+    const returnArray = [];
+    returnArray.push(["="+arrayOfValues.length]);
+    returnArray.push(["=AVERAGE(D9:E18)"]);
+    returnArray.push(["=SQRT(SUMPRODUCT((D9:E"+ (9+ (arrayOfValues.length/2 - 1)) +"-D22)^2)/(D21-1))"]);
+    returnArray.push(["=(D23*2)/SQRT(D21)"]);
+    returnArray.push(["=0.3*(1+5%)"]);
+    returnArray.push(["=SQRT(D24^2+D25^2)"]);
+
+    return returnArray;
+  };
+
+  const loadDataFromApp = ()=> {
+    const valuesToUse = findDataFromApp();
+    const newSheet = templateSheet.makeCopy('Medicion Hoy');
+    const editingSheet = SpreadsheetApp.openById(newSheet.getId()).getActiveSheet();
+
+    const valuesArray = [];
+   /* for (let i = 0; i < valuesToUse.length; i += 2) {
+        valuesArray.push([valuesToUse[i], valuesToUse[i + 1]]);
+    }*/
+    
+    
+    if(valuesToUse.length % 2 === 0)
+    {
+      for (let i = 0; i < valuesToUse.length/2; i ++) 
+        valuesArray.push([valuesToUse[i], valuesToUse[i + valuesToUse.length/2]]);
     }
+    else
+    {
+      for (let i = 0; i < valuesToUse.length/2; i ++) 
+        valuesArray.push([valuesToUse[i], valuesToUse[i + 1 + valuesToUse.length/2]]);
+      valuesArray.push([valuesToUse[valuesToUse.length]]);// deleted a 0 here
+    }
+    const calculatedData = insertarFormulas(valuesToUse);        
 
-    const loadSecondAttachment = ()=> {
-      secondAttachment = loadDataFromApp().getId();
-      let link = "https://docs.google.com/spreadsheets/d/" +secondAttachment+"/export"
-      const parametro = {
-      headers: {
-        "Authorization": 'Bearer ' + ScriptApp.getOAuthToken(),
-      },
-      method: 'GET',
-      muteHttpExceptions: true
-      }     
+    let range = editingSheet.getRange('D9:E14');
+    range.setValues(valuesArray);
+    range = editingSheet.getRange('D21:D26');
+    range.setFormulas(calculatedData);
+    SpreadsheetApp.flush();
+    const returnFile = DriveApp.getFileById(newSheet.getId());
+    return returnFile;
+  }
 
-      const response = UrlFetchApp.fetch(link,parametro);
-      const excelFile = response.getBlob().setName('Planilla cargada.xlsx');
-      return excelFile;
-    };
+  const loadSecondAttachment = ()=> {
+    secondAttachment = loadDataFromApp().getId();
+    let link = "https://docs.google.com/spreadsheets/d/" +secondAttachment+"/export"
+    const parametro = {
+    headers: {
+      "Authorization": 'Bearer ' + ScriptApp.getOAuthToken(),
+    },
+    method: 'GET',
+    muteHttpExceptions: true
+    }     
 
-    const deleteTemporaryFiles = (firstFile)=>{
-      Drive.Files.remove(firstFile.getId());
-      Drive.Files.remove(secondAttachment);
-    };
+    const response = UrlFetchApp.fetch(link,parametro);
+    const excelFile = response.getBlob().setName('Planilla cargada.xlsx');
+    return excelFile;
+  };
 
-    return {getFile,setFile,createCopyFile,findDataFromApp,loadSecondAttachment,deleteTemporaryFiles};
-  })();
+  const deleteTemporaryFiles = (firstFile)=>{
+    Drive.Files.remove(firstFile.getId());
+    Drive.Files.remove(secondAttachment);
+  };
+
+  return {getFile,setFile,createCopyFile,findDataFromApp,loadSecondAttachment,deleteTemporaryFiles};
+})();
 
 driveFolder.setFile('121GgIXCuNLle-bwcMdURSYDkBgF76bPz');
 
